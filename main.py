3d5a19ad191d853a11c601f5763d8f9f69827cd7
Juanma Coria <juanmc2005@hotmail.com>
2019-05-03 17:23:12 +0200

    Added script parameter to choose which loss to train

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from trainers import ContrastiveTrainer, ArcTrainer, SoftmaxTrainer, TripletTrai
 from distances import CosineDistance
 import argparse
 
-def arc_trainer():
-    return ArcTrainer(trainset, testset, device, nfeat=2, nclass=10)
 
-def contrastive_trainer():
-    return ContrastiveTrainer(trainset, testset, device, margin=0.25, distance=CosineDistance())
+def get_trainer(loss):
+    if loss == 'softmax':
+        return SoftmaxTrainer(trainset, testset, device)
+    elif loss == 'contrastive':
+        return ContrastiveTrainer(trainset, testset, device, margin=0.3, distance=CosineDistance())
+    elif loss == 'triplet':
+        return TripletTrainer(trainset, testset, device, margin=0.3, distance=CosineDistance())
+    elif loss == 'arcface':
+        return ArcTrainer(trainset, testset, device, nfeat=2, nclass=10)
+    else:
+        raise ValueError('Loss function should be one of: softmax / contrastive / triplet / arcface')
 
-def triplet_trainer():
-    return TripletTrainer(trainset, testset, device, margin=0.25, distance=CosineDistance())
-
-def softmax_trainer():
-    return SoftmaxTrainer(trainset, testset, device)
 
 # Config
 use_cuda = torch.cuda.is_available() and True
@@ -22,6 +24,7 @@ device = torch.device('cuda' if use_cuda else 'cpu')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mnist', type=str, help='Path to MNIST dataset')
+parser.add_argument('--loss', type=str, help='softmax / contrastive / triplet / arcface')
 args = parser.parse_args()
 torch.manual_seed(999)
 
@@ -33,6 +36,6 @@ trainset = datasets.MNIST(args.mnist, download=True, train=True, transform=trans
 testset = datasets.MNIST(args.mnist, download=True, train=False, transform=transform)
 
 # Train
-trainer = triplet_trainer()
-trainer.train(epochs=15, log_interval=90, train_accuracy=False)
+trainer = get_trainer(args.loss)
+trainer.train(epochs=15, log_interval=60, train_accuracy=False)
 
