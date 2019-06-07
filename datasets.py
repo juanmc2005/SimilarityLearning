Fixed batch dimension error in EER calculation. STS model can be trained with contrastive loss but without evaluation, and it's producing loss = nan

datasets.py
@@ -199,7 +199,7 @@ class SemEval(SimDataset):
             labels.append(tmp)
         return labels
 
-    def __init__(self, path, vector_path, vocab_path, batch_size, mode='classic', threshold=2.5):
+    def __init__(self, path, vector_path, vocab_path, batch_size, mode='baseline', threshold=2.5):
         # TODO mode parameter should be refactored into a strategy-like object
         self.path = path
         self.batch_size = batch_size
@@ -232,15 +232,15 @@ class SemEval(SimDataset):
             self.train_sents = self._pairs(atrain, btrain, simtrain, threshold)
         elif mode == 'triplets':
             self.train_sents = self._triplets(atrain, btrain, simtrain, threshold)
-        elif mode == 'classic':
+        elif mode == 'baseline':
             self.train_sents = np.array(list(zip(zip(atrain, btrain), simtrain)))
         else:
-            raise ValueError("Mode can only be 'classic', 'clusters', 'pairs' or 'triplets'")
+            raise ValueError("Mode can only be 'baseline', 'clusters', 'pairs' or 'triplets'")
 
     def training_partition(self):
         np.random.shuffle(self.train_sents)
         # TODO add other modes
-        if self.mode == 'classic':
+        if self.mode == 'baseline':
             return SemEvalPartition(self.train_sents, self.batch_size, classes=True, train=True)
         elif self.mode == 'pairs':
             return SemEvalPartition(self.train_sents, self.batch_size, classes=False, train=True)
@@ -264,7 +264,7 @@ class SemEval(SimDataset):
             sim = [float(line.strip()) for line in simfile.readlines()]
             for i in range(len(a)):
                 a[i], b[i] = self.pad_sent(a[i], b[i])
-            if partition == 'train' and self.mode == 'classic':
+            if partition == 'train' and self.mode == 'baseline':
                 sim = self.scores_to_probs(sim)
             return a, b, sim
 

distances.py
@@ -124,7 +124,7 @@ class EuclideanDistance(Distance):
         return 'euclidean'
 
     def dist(self, x, y):
-        return torch.dist(x, y, p=2)
+        return torch.sqrt(torch.sum(torch.pow(x - y, 2), 1))
     
     def sqdist_sum(self, x, y):
         return (x - y).pow(2).sum()

losses/base.py
@@ -182,7 +182,7 @@ class Evaluator(TrainingListener):
         self.batch_transforms = batch_transforms
         self.callbacks = callbacks
         self.feat_train, self.y_train = None, None
-        self.best_metric = 0
+        self.best_metric, self.best_epoch = 0, -1
     
     def _eval(self, model):
         model.eval()
@@ -235,9 +235,12 @@ class Evaluator(TrainingListener):
         metric_value = self.metric.get()
         print(f"--------------- Epoch {epoch:02d} Results ---------------")
         print(f"Test Metric: {metric_value:.6f}")
+        if self.best_epoch != -1:
+            print(f"Best until now: {self.best_metric:.6f}, at epoch {self.best_epoch}")
         print("------------------------------------------------")
         if metric_value > self.best_metric:
             self.best_metric = metric_value
+            self.best_epoch = epoch
             print('New Best Test Metric!')
             for cb in self.callbacks:
                 cb.on_best_accuracy(epoch, model, loss_fn, optim, metric_value, feat_test, y_test)
@@ -315,6 +318,7 @@ class BaseTrainer:
                 
             self.optim.zero_grad()
             loss.backward()
+            torch.nn.utils.clip_grad_norm(self.model.parameters(), 5)
             self.optim.step()
             
             for cb in self.callbacks:

models.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from torch import nn
 from sincnet import SincNet, MLP
-from pwim import PWIM
+from sts_baseline import STSBaselineNet
 
 
 class Flatten(nn.Module):
@@ -131,16 +131,16 @@ class SpeakerNet(SimNet):
 
 class SemanticNet(SimNet):
 
-    def __init__(self, device, nfeat, vector_vocab, pairwise=False, loss_module=None):
+    def __init__(self, device, nfeat, vector_vocab, loss_module=None, mode='baseline'):
         super().__init__(loss_module)
-        self.pwim = PWIM(device, nfeat_word=300, nfeat_sent=nfeat,
-                         vec_vocab=vector_vocab, tokens=vector_vocab.keys(), pairwise=pairwise)
+        self.base_model = STSBaselineNet(device, nfeat_word=300, nfeat_sent=nfeat,
+                                         vec_vocab=vector_vocab, tokens=vector_vocab.keys(), mode=mode)
 
     def layers(self):
-        return [self.pwim]
+        return [self.base_model]
 
     def common_state_dict(self):
-        return self.pwim.state_dict()
+        return self.base_model.state_dict()
 
     def load_common_state_dict(self, checkpoint):
-        self.pwim.load_state_dict(checkpoint)
+        self.base_model.load_state_dict(checkpoint)

sts_baseline.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 import numpy
 
 
-class PWIM(nn.Module):
-    def __init__(self, device, nfeat_word, nfeat_sent, vec_vocab, tokens, pairwise=False):
-        super(PWIM, self).__init__()
+class STSBaselineNet(nn.Module):
+    def __init__(self, device, nfeat_word, nfeat_sent, vec_vocab, tokens, mode='baseline'):
+        super(STSBaselineNet, self).__init__()
         self.device = device
         self.nfeat_word = nfeat_word
         self.nfeat_sent = nfeat_sent
-        self.pairwise = pairwise
+        self.mode = mode
         if 'oov' not in tokens:
             tokens.append('oov')
         self.word2id = {word: index for index, word in enumerate(tokens)}
@@ -31,12 +31,19 @@ class PWIM(nn.Module):
             else:
                 tmp.append(self.word2id['oov'])
         indices = Variable(torch.LongTensor(tmp)).to(self.device)
-        sentA = self.word_embedding(indices)
-        return sentA.view(-1, 1, self.nfeat_word)
+        embedded_sent = self.word_embedding(indices)
+        return embedded_sent.view(-1, 1, self.nfeat_word)
 
     def forward(self, sents):
-        if self.pairwise:
-            return self._forward_pairwise(sents)
+        # FIXME Replace this with subclasses or (preferably) delegation
+        if self.mode == 'baseline':
+            return self._forward_pair_concat(sents)
+        elif self.mode == 'pairs':
+            return self._forward_pair(sents)
+        else:
+            return self._forward_single(sents)
+
+    def _forward_single(self, sents):
         embeds = []
         for sent in sents:
             x = self.word_layer(sent)
@@ -47,7 +54,7 @@ class PWIM(nn.Module):
             embeds.append(embed)
         return torch.cat(embeds, 0).view(-1, self.nfeat_sent)
 
-    def _forward_pairwise(self, sents):
+    def _forward_pair_concat(self, sents):
         embeds = []
         for s1, s2 in sents:
             x1 = self.word_layer(s1)
@@ -61,3 +68,18 @@ class PWIM(nn.Module):
             embed = torch.cat((embed1, embed2), 1)
             embeds.append(embed)
         return torch.cat(embeds, 0).view(-1, 2*self.nfeat_sent)
+
+    def _forward_pair(self, sents):
+        embeds1, embeds2 = [], []
+        for s1, s2 in sents:
+            x1 = self.word_layer(s1)
+            x2 = self.word_layer(s2)
+            # Encode input
+            out1, _ = self.lstm(x1)
+            out2, _ = self.lstm(x2)
+            # Max pooling to get embeddings
+            embed1 = torch.max(out1, 0)[0]
+            embed2 = torch.max(out2, 0)[0]
+            embeds1.append(embed1)
+            embeds2.append(embed2)
+        return torch.cat(embeds1, 0).view(-1, self.nfeat_sent), torch.cat(embeds2, 0).view(-1, self.nfeat_sent)