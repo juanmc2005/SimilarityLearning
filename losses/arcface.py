Fix: '+=' operation in arcface cutting the gradient because of 'in-place operation'. Other minor changes

losses/arcface.py
@@ -6,15 +6,19 @@ import torch.nn.functional as F
 
 
 class ArcLinear(nn.Module):
+    """Additive Angular Margin linear module (ArcFace)
+    Parameters
+    ----------
+    nfeat : int
+        Embedding dimension
+    nclass : int
+        Number of classes
+    margin : float
+        Angular margin to penalize distances between embeddings and centers
+    s : float
+        Scaling factor for the logits
     """
-    Additive Angular Margin linear layer module (ArcFace)
-    Reference: https://arxiv.org/pdf/1801.07698.pdf
-    :param nfeat: the number of features in the embedding
-    :param nclass: the number of classes
-    :param margin: the margin to separate classes in angular space
-    :param s: the scaling factor for the feature vector
-    """
-    
+
     def __init__(self, nfeat, nclass, margin, s):
         super(ArcLinear, self).__init__()
         eps = 1e-4
@@ -25,48 +29,36 @@ class ArcLinear(nn.Module):
         self.s = s
         self.W = nn.Parameter(torch.Tensor(nclass, nfeat))
         nn.init.xavier_uniform_(self.W)
-    
-    def forward(self, x, y):
-        """
-        Apply the angular margin transformation
-        :param x: a feature vector batch
-        :param y: a non one-hot label batch
-        :return: a tensor of logits with the angular margin transformation
-        """
-        # Normalize the feature vectors and W
-        xnorm = F.normalize(x)
-        Wnorm = F.normalize(self.W)
-        y = y.long().view(-1, 1)
-        # Calculate cosθj (the logits)
-        cos_theta_j = torch.matmul(xnorm, torch.transpose(Wnorm, 0, 1))
-        # Get the cosθ corresponding to the classes
-        cos_theta_yi = cos_theta_j.gather(1, y)
-        # For numerical stability
-        cos_theta_yi = cos_theta_yi.clamp(min=self.min_cos, max=self.max_cos)
-        # Get the angle separating xi and Wyi
-        theta_yi = torch.acos(cos_theta_yi)
-        # Apply the margin to the angle
-        cos_theta_yi_margin = torch.cos(theta_yi + self.margin)
-        # One hot encode  y
-        one_hot = torch.zeros_like(cos_theta_j)
-        one_hot.scatter_(1, y, 1.0)
-        # Project margin differences into cosθj
-        cos_theta_j += one_hot * (cos_theta_yi_margin - cos_theta_yi)
-        # Apply the scaling
-        cos_theta_j = self.s * cos_theta_j
-        return cos_theta_j
 
-    def predict(self, x):
+    def forward(self, x, target=None):
+        """Apply the angular margin transformation
+        Parameters
+        ----------
+        x : `torch.Tensor`
+            an embedding batch
+        target : `torch.Tensor`
+            a non one-hot label batch
+        Returns
+        -------
+        fX : `torch.Tensor`
+            logits after the angular margin transformation
         """
-        Apply the angular margin transformation
-        :param x: a feature vector batch
-        :return: a tensor of logits
-        """
-        # Normalize the feature vectors and W
+        # normalize the feature vectors and W
         xnorm = F.normalize(x)
         Wnorm = F.normalize(self.W)
-        # Calculate cosθj (the logits)
+        target = target.long().view(-1, 1)
+        # calculate cosθj (the logits)
         cos_theta_j = torch.matmul(xnorm, torch.transpose(Wnorm, 0, 1))
-        # Apply the scaling
-        cos_theta_j = self.s * cos_theta_j
-        return cos_theta_j
+        # get the cosθ corresponding to the classes
+        cos_theta_yi = cos_theta_j.gather(1, target)
+        # for numerical stability
+        cos_theta_yi = cos_theta_yi.clamp(min=self.min_cos, max=self.max_cos)
+        # get the angle separating xi and Wyi
+        theta_yi = torch.acos(cos_theta_yi)
+        # apply the margin to the angle
+        cos_theta_yi_margin = torch.cos(theta_yi + self.margin)
+        # one hot encode  y
+        one_hot = torch.zeros_like(cos_theta_j)
+        one_hot.scatter_(1, target, 1.0)
+        # project margin differences into cosθj
+        return self.s * (cos_theta_j + one_hot * (cos_theta_yi_margin - cos_theta_yi))