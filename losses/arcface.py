bf2e6d623ea9891817ca80d16c135244cdbda599
juan coria <coria@limsi.fr>
2020-04-08 12:18:59 +0200

    ArcLinear module can now be used during evaluation without applying the margin

--- losses/arcface.py
+++ losses/arcface.py
@@ -46,19 +46,23 @@ class ArcLinear(nn.Module):
         # normalize the feature vectors and W
         xnorm = F.normalize(x)
         Wnorm = F.normalize(self.W)
-        target = target.long().view(-1, 1)
         # calculate cosθj (the logits)
         cos_theta_j = torch.matmul(xnorm, torch.transpose(Wnorm, 0, 1))
-        # get the cosθ corresponding to the classes
-        cos_theta_yi = cos_theta_j.gather(1, target)
-        # for numerical stability
-        cos_theta_yi = cos_theta_yi.clamp(min=self.min_cos, max=self.max_cos)
-        # get the angle separating xi and Wyi
-        theta_yi = torch.acos(cos_theta_yi)
-        # apply the margin to the angle
-        cos_theta_yi_margin = torch.cos(theta_yi + self.margin)
-        # one hot encode y
-        one_hot = torch.zeros_like(cos_theta_j)
-        one_hot.scatter_(1, target, 1.0)
-        # project margin differences into cosθj
-        return self.s * (cos_theta_j + one_hot * (cos_theta_yi_margin - cos_theta_yi))
+        if self.training:
+            target = target.long().view(-1, 1)
+            # get the cosθ corresponding to the classes
+            cos_theta_yi = cos_theta_j.gather(1, target)
+            # for numerical stability
+            cos_theta_yi = cos_theta_yi.clamp(min=self.min_cos, max=self.max_cos)
+            # get the angle separating xi and Wyi
+            theta_yi = torch.acos(cos_theta_yi)
+            # apply the margin to the angle
+            cos_theta_yi_margin = torch.cos(theta_yi + self.margin)
+            # one hot encode y
+            one_hot = torch.zeros_like(cos_theta_j)
+            one_hot.scatter_(1, target, 1.0)
+            # project margin differences into cosθj
+            return self.s * (cos_theta_j + one_hot * (cos_theta_yi_margin - cos_theta_yi))
+        else:
+            # don't penalize the correct class when evaluating the model
+            return self.s * cos_theta_j
